d6b0ee45a20743e41c632ebd48fab1ad59df5bbb
senadkurtisi <senad98kurtisi@gmail.com>
2021-11-07 10:45:19 +0100

    Add data column expansion

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 
+import numpy as np
 import pandas as pd
 
 
@@ -22,3 +23,25 @@ def load_dataset(dataset_path):
     
     df = pd.DataFrame(event_list_decoded)
     return df
+
+
+def unflatten_event_data(df, target_columns, data_column="event_data"):
+    """Unravels the df's data column to multiple columns.
+    
+    This columns is stored as a JSON object therefore this is possible.
+
+    Arguments:
+        df (pandas.DataFrame): DataFrame which contains data column
+        target_columns (list of str): All columns that should come out of the data column
+    Returns:
+        df_copy (pandas.DataFrame): DataFrame with @data_column expanded to multiple other
+    """
+    df_copy = df.copy()
+    # Not all events will be valid, i.e. some events may lack some of the required data
+    # specified in the @target_columns argument. These events will be dropped later
+    for col in target_columns:
+        df_copy[col] = np.NaN
+
+    # Decode the data column
+    df_copy[target_columns] = df[data_column].apply(lambda x: pd.Series(x))
+    return df_copy
