f05e8075e889f40a363bbe0aa2e0f07ec76876b5
senadkurtisi <senad98kurtisi@gmail.com>
2021-11-07 14:43:15 +0100

    Added argument parser

--- process_data.py
+++ process_data.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import json
 
 import sqlite3
@@ -6,7 +7,18 @@ from utils import load_dataset, clean_data, process_club_stats
 from database_utils import save_club_stats
 
 
+def parse_args():
+    parser = ArgumentParser(description="Loads and cleans the dataset. Saves processed data to a database.")
+    parser.add_argument("-d", "--dataset_path", type=str,
+                        help="Path where the '.jsonl' dataset file is stored.")
+    args = parser.parse_args()
+    return args
+
+
 def main():
+    # Retrieve command line arguments
+    args = parse_args()
+
     config_file_path = ".\\config.json"
     with open(config_file_path, "r") as cfg_file:
         config = json.load(cfg_file)
@@ -15,7 +27,7 @@ def main():
     db_conn = sqlite3.connect(config["database_name"])
 
 
-    df_dataset = load_dataset(config["dataset_path"])
+    df_dataset = load_dataset(args.dataset_path)
     df_matches, df_goals = clean_data(df_dataset, config)
     club_stats = process_club_stats(df_matches, df_goals, config["match_id_column"])
 
